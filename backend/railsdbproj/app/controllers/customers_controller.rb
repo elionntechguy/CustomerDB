715c4fedd8c2e7f451f27956054551cb06353917
elionntechguy <41442942+elionntechguy@users.noreply.github.com>
2021-11-06 23:22:33 +0100

    Fixed table-responsive extra whitespace bug

--- backend/railsdbproj/app/controllers/customers_controller.rb
+++ backend/railsdbproj/app/controllers/customers_controller.rb
@@ -5,11 +5,12 @@ class CustomersController < ApplicationController
     # @user = @customers.pluck(:assignedusersid).flatten.map { |id| User.find(id) }
     @user = User.where(id: @customers.pluck(:assignedusersid).flatten)
     username = @user.map { |all| { user_id: all.id, customers_id: all.assignedcustomersid, username: all.username } }
-    render json: { customers: @customers, assigneduser: username }  
+    render json: { customers: @customers, assigneduser: username }
   end
 
   def new_customer
-    @customer = Customer.new(name: params[:name], email: params[:email], phonenumber: params[:phonenumber], issues: [params[:issues]], issue_description: params[:issue_description], assignedusersid: [current_user.id])
+    @customer = Customer.new(name: params[:name], email: params[:email], phonenumber: params[:phonenumber], issues: [params[:issues]], issue_description: params[:issue_description],
+                             assignedusersid: [current_user.id])
     @customer.issue_description = "//" if params[:issue_description].blank?
 
     if @customer.save
@@ -17,7 +18,7 @@ class CustomersController < ApplicationController
       @user.update(assignedcustomersid: @user.assignedcustomersid.push(@customer.id))
       render json: { message: "Successfully created customer #{@customer.name}" }, status: :created
     else
-      render json: {error: @customer.errors, error1: @user.errors}, status: :unprocessable_entity
+      render json: { error: @customer.errors, error1: @user.errors }, status: :unprocessable_entity
     end
   end
 
